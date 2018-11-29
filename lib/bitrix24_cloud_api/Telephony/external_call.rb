2d8c5dfe907cc1f5c6e0fd15696eb33ab1530e9b
Maxim Shirokov <maximshirokov21@gmail.com>
2018-11-29 16:29:54 +0500

    telephony module ads and fixes

---
+++ lib/bitrix24_cloud_api/Telephony/external_call.rb
@@ -0,0 +1,21 @@
+module Bitrix24CloudApi
+  module TELEPHONY
+    # Bitrix24 telephony API
+    module EXTERNALCALL
+      def self.const_missing(c)
+        target_class = Bitrix24CloudApi::Telephony.const_get(c)
+        target_class.define_singleton_method(:resource_path) { "telephony.#{c.downcase}" }
+        target_class
+      end
+    end
+
+    # Bitrix24 telephony external call API
+    class ExternalCall < Bitrix24CloudApi::Telephony
+      %i[registe finish].each do |action|
+        define_singleton_method(action) do |client, query = {}|
+          client.make_get_request(resource_url(client, action), query)
+        end
+      end
+    end
+  end
+end
